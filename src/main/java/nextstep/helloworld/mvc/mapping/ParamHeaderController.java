a68717e58023841b76932f01d9766aa005d8e290
kukaro <justkukaro@naver.com>
2021-01-15 05:40:04 +0900

    feat : 여러가지 예제 테스트

--- src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
@@ -1,5 +1,6 @@
 package nextstep.helloworld.mvc.mapping;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/param-header")
 public class ParamHeaderController {
 
+    @GetMapping(value = "/message")
     public ResponseEntity<String> message() {
         return ResponseEntity.ok().body("message");
     }
 
+    @GetMapping(value = "/message", params = "name=hello")
     public ResponseEntity<String> messageForParam() {
         return ResponseEntity.ok().body("hello");
     }
 
+    @GetMapping(value = "/message", headers = "HEADER=hi")
     public ResponseEntity<String> messageForHeader() {
         return ResponseEntity.ok().body("hi");
     }
